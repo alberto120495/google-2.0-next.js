b8319a282f2a1981056f83f529cf68e109e7a7ef
AlbertoWindows <albertopimentel04@gmail.com>
2021-04-09 20:10:32 -0500

    app build

--- pages/search.js
+++ pages/search.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import { API_KEY, CONTEXT_KEY } from "../keys";
-import Response from "../Response";
+//import Response from "../Response";
 import { useRouter } from "next/router";
 import SearchResults from "../components/SearchResults";
 function Search({ results }) {
@@ -21,13 +21,11 @@ function Search({ results }) {
 export default Search;
 
 export async function getServerSideProps(context) {
-  const useDummyData = false;
+  // const useDummyData = false;
   const startIndex = context.query.start || "0";
-  const data = useDummyData
-    ? Response
-    : await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
-      ).then((response) => response.json());
+  const data = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+  ).then((response) => response.json());
 
   //After the SERVER has rendered ... pass the results to the client
 
